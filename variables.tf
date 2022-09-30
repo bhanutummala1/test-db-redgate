a435d627a5e2a4fe9542cd0af6fa467f5cd1a59d
Bhanu Tummala <BhanuTummala@randallreilly.com>
2022-09-30 10:11:23 -0500

    t commit.

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "RR_MP_REGION"{
+  type = string
+  default = "us-east-2"
+}
+
+variable "RR_MP_AWS_PROFILE"{
+  type = string 
+  default = "default"
+}
+
+variable "access_key" {
+  description = "access_key"
+  type = string 
+  default = "ASIA2DA74ZRISIU5WRWN"
+}
+
+variable "secret_key" {
+  description = "secret_key"
+  type = string 
+  default = "Mwala5CzpVlxxjTNKxmvJG2xr+L6gWpDH8XAspu0"
+}
+variable "aws_session_token" {
+  description = "session_token"
+  type = string 
+  default = "IQoJb3JpZ2luX2VjEC8aCXVzLWVhc3QtMSJIMEYCIQDl8xMTWosuY6vY9+2KG6tEydsKLchVrNiH2lLKqGR8LAIhAPjkGEgFC8gH0T++ipwGj3ZAYnW6sFSlBoUlIkKZhyiFKu8BCEgQABoMNjkzNzA0MTE3MzI5IgzaRzgpbkGAhdqcjdMqzAFf7/77mulIUb8Ae5xNj3bV3xPG8cwBvUU09nuBHTFCTLnG7HN6yB13+U4ATpNWJzVskPxv4laH2xUf+apMbWLlUngXsPaAtJxKFXSawWTO91p0Dm1VIeUwHFAKA5TH+yMPFGae+PaUe3r9WpU3yitHHm6lMYHc9j5gBvpbF6jkxKkvyTJO7Vyp5SbXVD/husK9xe2qn9/s8XrRgKsSMUckMqV9FQ4UppzfpGG9K/uJMHZlv3aNxfpO7oD/cVxzwoSnANwTy1vr8vplRWMwgq7HlQY6lwF7yytY2RVh3Hf+KKEri3XBPl7sOd4WmAwQYma5zzFKGQnQr4gw/8AVOzC55E72x7hdKx/ePmSKCPveRWHVmBv/pRkVp4miWdKv4yjRxWVFZnOSCSRfP2ftppZLRPIrWDqazuuCM6T3ZGTjOfF4BQerGX4twyKWt1CO0b68wXNHyvIebVrgmd8KItIDyHyhhWbZwhMMjx22"
+}
